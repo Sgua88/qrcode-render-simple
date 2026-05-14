a58be5348ed395c56cdd8e1a8fe8eace43213538
Sgua88 <velocity999@hotmail.it>
2026-05-14 23:46:28 +0200

    Delete scripts directory

--- scripts/sync-once.js
+++
@@ -1,4 +0,0 @@
-require("dotenv").config();
-const { initDb, pool } = require("../src/db");
-const { runSync } = require("../src/sync");
-(async()=>{ await initDb(); await runSync({manual:true}); await pool.end(); })();
